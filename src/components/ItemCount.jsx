25cdeac3b01e36c1478a6356af2fad2de3856de3
Gabot3ck <jesus.castilloc@outlook.com>
2022-06-20 20:56:15 -0300

    Se midifica los estados en ItemCount y ItemDetail

--- src/components/ItemCount.jsx
+++ src/components/ItemCount.jsx
@@ -1,10 +1,12 @@
+import { useState } from "react";
 
-const ItemCount = ({count, setCount, stock, onAdd})=> {
-    
+const ItemCount = ({initial, stock, onAdd})=> {
+
+    const [cantidad, setCantidad] = useState(initial);
 
     const sumar = () => {
         if(stock > 0 ){
-            count < stock ? setCount(count + 1) : alert("No se pueden agregar más productos")
+            cantidad < stock ? setCantidad(cantidad + 1) : alert("No se pueden agregar más productos")
         } else {
             alert("Producto sin stock, no se puede agregar al carrito.")
         }
@@ -12,16 +14,16 @@ const ItemCount = ({count, setCount, stock, onAdd})=> {
     }
 
     const restar = () => {
-        count  > 1 ? setCount(count - 1) : alert("No se pueden eliminar más productos")
+        cantidad  > 1 ? setCantidad(cantidad - 1) : alert("No se pueden eliminar más productos")
     }
 
     
     return (
         <div className="w-100 mx-auto  text-center p-1">
-            <h2 className="">{count}</h2>
+            <h2 className="">{cantidad}</h2>
             <div className="d-flex justify-content-center align-items-center ">
                 <button className="btn btn-secondary" onClick={restar}>-</button>
-                <button className="my-3 mx-2 btn btn-primary" onClick={()=> {onAdd(count)}}>Agregar al carrito</button>
+                <button className="my-3 mx-2 btn btn-primary" onClick={()=> {onAdd(cantidad)}}>Agregar al carrito</button>
                 <button className="btn btn-secondary" onClick={sumar}>+</button>
             </div>
             <br />
--- src/components/ItemDetail.jsx
+++ src/components/ItemDetail.jsx
@@ -1,11 +1,10 @@
 import ItemCount from "./ItemCount";
-import {useState} from "react";
-import {Link} from 'react-router-dom';
+import { useState } from "react";
+import { Link } from "react-router-dom";
 
 function ItemDetail({ item }) {
     const { titulo, image, stock, precio } = item;
 
-    const [cantidad, setCantidad] = useState(1);
     const [mostrar, setMostrar] = useState(true);
 
     const onAdd = (count) => {
@@ -19,13 +18,12 @@ function ItemDetail({ item }) {
         <div className="card-body text-center">
             <h5 className="card-title">{titulo}</h5>
             <h6 className="card-subtitle mb-2  text-danger ">Precio: $CLP {precio}</h6>
-            { mostrar ? <ItemCount  stock={stock} onAdd={onAdd} count={cantidad} setCount={setCantidad}/> : <Link to={`/cart`} class="btn btn-primary">Ir a carrito</Link>}
+            { mostrar ? <ItemCount  stock={stock} onAdd={onAdd} initial={1}/> : <Link to={`/cart`} class="btn btn-primary">Ir a carrito</Link>}
         </div>
     </div>
     
     </>);
 
-    
 }
 
 export default ItemDetail;
