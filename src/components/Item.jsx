1b2d9aebbe689924b38b2aecfe0a5fc447bc167e
Gabot3ck <jesus.castilloc@outlook.com>
2022-07-09 19:46:52 -0300

    Se da estilos al Item

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -2,16 +2,18 @@ import "./Item.css"
 import { Link } from "react-router-dom";
 
 function Item({product}) {
-    const {titulo,image,id} = product
+    const {titulo,image,id,precio, stock} = product
     
     return(
         <>
         <div className="col-12 col-sm-6 col-md-4  col-lg-3 d-flex justify-content-center">
             <div className="card mx-2 mb-4 mt-1 " >
-                <img src={image} className="card-img-top imagen mx-auto img-fluid" alt="..."></img>
-                <div className="card-body">
+                <img src={image} className="card-img-top imagen mx-auto" alt="..."></img>
+                <div className="card-body bg-dark d-flex flex-column justify-content-between">
                     <h5 className="card-title">{titulo}</h5>
-                    <Link to={`/item/${id}`} class="btn btn-primary">Abrir</Link>
+                    <p className="precio-card">$CLP {precio}</p>
+                    <Link to={`/item/${id}`} class="btn btn-warning">Ver detalles</Link>
+                    <p className="stock-card p-0 m-0">Stock: {stock} uds.</p>
                 </div> 
             </div>
         </div>
